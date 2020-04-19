d1fb3d690c760a50c1a220c1623495d77d0b46d4
JamesLambe3006 <james.lambe@bglgroup.co.uk>
2020-04-19 14:45:18 +0100

    Add project files.

---
+++ Generics/GenericPrinter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Generics
+{
+    class GenericPrinter<T>
+    {
+        private T _firstProperty;
+        private T _secondProperty;
+
+        public GenericPrinter(T firstProperty, T secondProperty)
+        {
+            _firstProperty = firstProperty;
+            _secondProperty = secondProperty;
+        } 
+
+        public void Print()
+        {
+            Console.WriteLine($"First property is: {_firstProperty}");
+            Console.WriteLine($"Second property is: {_secondProperty}");
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericPrinter<int> intobject = new GenericPrinter<int>(10, 20);
+
+            intobject.Print();
+
+            GenericPrinter<string> stringobject = new GenericPrinter<string>("Generic Method", "Generic Class");
+
+            stringobject.Print();
+
+            Console.Read();
+        }
+    }
+}
